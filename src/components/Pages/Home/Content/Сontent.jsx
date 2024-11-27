a2d7c72a869169f4cd05b2deb7e56d8a1dd17a34
MisterGhost <Volodymyr.Velychko@lizenza.com>
2024-11-27 17:35:01 +0200

    add statistics components

--- src/components/Pages/Home/Content/Сontent.jsx
+++ src/components/Pages/Home/Content/Сontent.jsx
@@ -1,11 +1,13 @@
 import { Fragment} from "react";
 import GetStarted from "./GetStarted/GetStarted";
+import Statistics from "./Statistics/Statistics";
 
 export default function Content() 
 {
   return (
     <Fragment>
         <GetStarted/>
+        <Statistics/>
     </Fragment>
   );
 }
