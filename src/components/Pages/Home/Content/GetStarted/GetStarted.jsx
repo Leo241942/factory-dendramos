f63dd9b97e282126781a56f1b689cd014071b787
MisterGhost <Volodymyr.Velychko@lizenza.com>
2024-11-27 07:12:55 +0200

    add componets "GetStarted" and  "Content"

--- src/components/Pages/Home/Content/GetStarted/GetStarted.jsx
+++ src/components/Pages/Home/Content/GetStarted/GetStarted.jsx
@@ -1,8 +1,21 @@
+import './GetStarted.css';
+
 export default function GetStarted() 
 {
   return (
-    <div>
-        
+    <div className="getstarted-Container">
+     
+      <div className="getstarted-block">
+        <div className="getstarted-text">
+           <p className="easyuse-text">Want anything to be easy with LaslesVPN.</p>
+           <p className="provide-text">Provide a network for all your needs with ease and fun using LaslesVPN discover interesting features from us.</p>
+            <div>
+              <a className="getstarted-button"  href='#'>Get Started</a>
+            </div>
+        </div>
+      </div>
+
+      <img className="getstarted-image" srcSet="/images/human.png" alt='human use vpn'/>
     </div>
   );
 }
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
+import { Fragment} from "react";
 import Header from "../../Header/Header";
+import Content from "./Content/Сontent";
 
 export default function HOME() 
 {
   return (
-    <>
+   <Fragment>
       <Header/>
-      
-    </>
+      <Content/>
+    </Fragment>
   );
 }
